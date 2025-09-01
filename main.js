7e6f16ee1073b780dbd5e02a401e5f8b3d04224e
Roberto Castillo <roberto.castnj@gmail.com>
2025-09-01 14:56:58 -0600

    Se agrego la función de promedios bajos

--- main.js
+++ main.js
@@ -38,9 +38,19 @@ const estudiantes = [
 
 function obtenerProm(arrayObjetos) {
   let sum = 0;
-  arrayObjetos.forEach((element) => {
-    sum += element.precio;
+  arrayObjetos.forEach((nota) => {
+    sum += nota;
   });
 
   return sum / arrayObjetos.length;
 }
+
+function filtrarPorPromedioBajo(arrayObjetos) {
+  return arrayObjetos.filter((elemento) => {
+    const promedio = obtenerProm(elemento.notas);
+    return promedio < 51;
+  });
+}
+
+const estudiantesConPromedioBajo = filtrarPorPromedioBajo(estudiantes);
+console.log(estudiantesConPromedioBajo);
